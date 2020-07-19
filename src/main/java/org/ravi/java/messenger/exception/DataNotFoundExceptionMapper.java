7d3576b10e77dec38f1df6b2741f6d2a78d57f6b
Ravi Bhavsar <rbhavsar@birst.com>
2020-07-19 13:08:55 -0700

    Messanger App + Jersey + Rest Api

---
+++ src/main/java/org/ravi/java/messenger/exception/DataNotFoundExceptionMapper.java
@@ -0,0 +1,21 @@
+package org.ravi.java.messenger.exception;
+
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+import org.ravi.java.messenger.model.ErrorMessage;
+
+@Provider
+public class DataNotFoundExceptionMapper implements ExceptionMapper<DataNotFoundException> {
+
+	@Override
+	public Response toResponse(DataNotFoundException ex) {
+		ErrorMessage errorMessage = new ErrorMessage(ex.getMessage(), 404, "http://java.ravi.org");
+		return Response.status(Status.NOT_FOUND)
+				.entity(errorMessage)
+				.build();
+	}
+
+}
